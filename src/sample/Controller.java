f47c73cf4c6fc84ae9eba1f0e929cf00ca672fe8
kohmab <kohmab@gmail.com>
2020-01-30 14:17:07 +0300

    garbage removed button disappears on click

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,18 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
+
+    @FXML
+    private Button plotButton;
+
+    @FXML
+    private void plot(ActionEvent event) {
+        event.consume();
+        plotButton.setVisible(false);
+        System.out.println("... and it disappeared! ");
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,14 +6,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.control.Button;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        Button runButton = new Button("Plot!");
         primaryStage.setTitle("Lets plot!");
         primaryStage.setScene(new Scene(root, 777, 777));
         primaryStage.show();
