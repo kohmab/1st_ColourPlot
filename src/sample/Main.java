2a14483fa0569c5f53aef82603e43099d1aa9021
kohmab <kohmab@gmail.com>
2020-01-30 13:57:30 +0300

    Button placed

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,19 +1,23 @@
 package sample;
 
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Button runButton = new Button("Plot!");
+        primaryStage.setTitle("Lets plot!");
+        primaryStage.setScene(new Scene(root, 777, 777));
         primaryStage.show();
+
     }
 
 
