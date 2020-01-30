69f13c73633313496e18d15b13dff2124ef9f2cc
kohmab <kohmab@gmail.com>
2020-01-30 17:04:37 +0300

    set resizable false

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -17,6 +17,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Lets plot!");
         primaryStage.setScene(new Scene(root, WIDTH, HEIGHT));
+        primaryStage.setResizable(false);
         primaryStage.show();
 
     }
